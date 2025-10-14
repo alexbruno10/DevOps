b91eb1469526a970942b834cff05e078131e8491
Álex Bruno Neri Humberto <alexbruno1298@hotmail.com>
2025-10-14 19:37:44 -0300

    Update test_Calculator.cpp

--- calculator/tests/test_Calculator.cpp
+++ calculator/tests/test_Calculator.cpp
@@ -5,23 +5,23 @@
 TEST(TestCalculator, Integer)
 {
     Calculator<int> c (10, 10);
-    EXPECT_EQ (c.add(), 20);
-    EXPECT_EQ (c.subtract(), 0);
-    EXPECT_EQ (c.multiply(), 100);
-    EXPECT_EQ (c.divide(), 1);
+    EXPECT_EQ(c.add(), 20);
+    EXPECT_EQ(c.subtract(), 0);
+    EXPECT_EQ(c.multiply(), 100);
+    EXPECT_EQ(c.divide(), 1);
 
     Calculator<int> c2 (0, 0);
-    EXPECT_EQ (c2.divide(), 0);
+    EXPECT_THROW(c2.divide(), std::runtime_error);  // captura exceção
 }
 
 TEST(TestCalculator, Double)
 {
     Calculator<double> c (10.0, 0.5);
-    EXPECT_EQ (c.add(), 10.5);
-    EXPECT_EQ (c.subtract(), 9.5);
-    EXPECT_EQ (c.multiply(), 5);
-    EXPECT_EQ (c.divide(), 20);
+    EXPECT_EQ(c.add(), 10.5);
+    EXPECT_EQ(c.subtract(), 9.5);
+    EXPECT_EQ(c.multiply(), 5);
+    EXPECT_EQ(c.divide(), 20);
 
     Calculator<double> c2 (0.0, 0.0);
-    EXPECT_EQ (c2.divide(), 0);
+    EXPECT_THROW(c2.divide(), std::runtime_error);  // captura exceção
 }
