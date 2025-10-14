81a9cf784721bd384ddcefcf47eae57fae3a8c4e
Álex Bruno Neri Humberto <alexbruno1298@hotmail.com>
2025-10-14 19:34:40 -0300

    Update calculator.hpp

--- calculator/src/calculator.hpp
+++ calculator/src/calculator.hpp
@@ -13,5 +13,11 @@ class Calculator {
 
   inline T multiply() { return number1 * number2; }
 
-  inline T divide() { return number1 / number2; }
+    inline T divide() {
+      if (number2 == 0) {
+          throw std::runtime_error("Divisão por zero!");
+      }
+      return number1 / number2;
+  }
+
 };
